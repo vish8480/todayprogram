727e91e97df8567e74a9132431375ddb18e72c3f
vish8480 <117697541+vish8480@users.noreply.github.com>
2022-11-18 07:39:24 +0530

    this is css code

---
+++ mystyle.css
@@ -0,0 +1,18 @@
+<html>  
+<head>  
+<style>  
+h1{  
+color:white;  
+background-color:red;  
+padding:5px;  
+}  
+p{  
+color:blue;  
+}  
+</style>  
+</head>  
+<body>  
+<h1>Write Your First CSS Example</h1>  
+<p>This is Paragraph.</p>  
+</body>  
+</html>  
